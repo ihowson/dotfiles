4bf21a9451acf4ca6f62e00cdafaeeb9180b38a4
Ian Howson <ian@mutexlabs.com>
2017-06-28 10:30:47 +0200

    Miscellaneous tweaks from deployment experience

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 karabiner.conf goes in ~/.karabiner.d/configuration
 macos.sh gets run on new installs
 
+
+keyboard->Modifier Keys; switch caps lock to escape
+Mouse: enable tap to click
+Mouse: increase tracking speed
+Mouse: turn off ‘natural’ scroll direction
+Mouse: disable all scroll and zoom gestures
+
+Accessibility->Display->Cursor size: increase to about 20%
+
--- mac.sh
+++ mac.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+# speed up key repeat and decrease initial delay
+# https://apple.stackexchange.com/a/83923
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
 
-defaults write -g QLPanelAnimationDuration -float 0
 
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
-
-defaults write com.apple.finder DisableAllAnimations -bool true
-
-defaults write com.apple.dock launchanim -bool false
-
-defaults write com.apple.dock expose-animation-duration -float 0.1
-
-defaults write com.apple.Dock autohide-delay -float 0
-
+# below this line, not reviewed by Ian
+#defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+#defaults write -g QLPanelAnimationDuration -float 0
+#defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+#defaults write com.apple.finder DisableAllAnimations -bool true
+#defaults write com.apple.dock launchanim -bool false
+#defaults write com.apple.dock expose-animation-duration -float 0.1
+#defaults write com.apple.Dock autohide-delay -float 0
 #defaults write NSGlobalDomain KeyRepeat -int 0
 
