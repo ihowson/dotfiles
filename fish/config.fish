56751003fc8e24fe7f7a3faa0cf43643b5860f6e
Ian Howson <ian@mutexlabs.com>
2017-07-05 04:49:01 +0200

    Move to virtualfish

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,4 @@ set -gx PATH ~/bin /usr/local/sbin $PATH
 set fish_user_paths /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
 
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
-
+eval (python -m virtualfish auto_activation)
