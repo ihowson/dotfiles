ed2743ed1bd6fa9cb676a770f0fe984c0094a8da
Ian Howson <ian@mutexlabs.com>
2018-09-21 10:13:59 -0700

    Snapshot 180921

--- fish/config.fish
+++ fish/config.fish
@@ -12,9 +12,12 @@ set -gx PATH ~/bin ~/nv/bin /usr/local/sbin /Users/ian/Library/Python/3.6/bin/ $
 set fish_user_paths /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
 
-eval (python3 -m virtualfish auto_activation)
+#eval (python3 -m virtualfish auto_activation)
 
 export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
 
 alias gs="git status"
+alias ls="exa"
+alias cat="bat"
+alias find="echo use fd"
 
